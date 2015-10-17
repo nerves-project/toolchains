523a664ff305f1a209c1c9d87131ecd2f2fdd07f
Frank Hunleth <fhunleth@troodon-software.com>
2015-10-17 03:04:15 -0400

    Add initial scripts and toolchain configs

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # nerves-toolchain
-Configuration and scripts to build the cross-compilers used by Nerves
+
+This project contains the configuration and scripts used to build the
+cross-compilers for the Nerves project. While pre-built toolchains exist for
+various hosts and targets, they don't seem to exist for the combination
+supported by Nerves. This project fills that gap.
+
+## Linux
+
+Install the following packages:
+
+```
+sudo apt-get install TBD
+```
+
+Run `build-linux.sh` and wait.
+
+## OSX
+
+Install the following packages:
+
+```
+brew update
+brew tap homebrew/dupes
+brew install gawk binutils xz wget automake
+brew install libtool autoconf gnu-sed mpfr gmp gcc
+```
+
+Run `build-osx.sh` and wait.
+
+## Windows
+
+TBD
+
---
+++ build-linux.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+CTNG_TAG=625f7e66b43c8629c7ca27b062ff7adad9c2b859
+BASE_DIR=`pwd`
+WORK_DIR=$BASE_DIR/work
+
+NERVES_TOOLCHAIN_TAG=`git describe --always --dirty`
+HOST_ARCH=`uname -m`
+
+# Clean up an old build
+rm -fr $WORK_DIR
+
+mkdir -p $WORK_DIR
+mkdir -p $WORK_DIR/usr
+
+# Build and install ct-ng to the work directory
+cd $WORK_DIR
+git clone https://github.com/crosstool-ng/crosstool-ng.git
+cd crosstool-ng
+git checkout $CTNG_TAG
+
+./bootstrap
+echo ./configure --prefix=$WORK_DIR/usr
+./configure --prefix=$WORK_DIR/usr
+make
+make install
+
+# Build the toolchain
+mkdir -p $WORK_DIR/build
+cd $WORK_DIR/build
+cp $BASE_DIR/configs/linux.config .config
+$WORK_DIR/usr/bin/ct-ng build
+
+# Figure out the target's tuple. It's the name of the only directory.
+cd $WORK_DIR/x-tools
+TARGET_TUPLE=`ls`
+
+# Clean up the build product
+rm -f $TARGET_TUPLE/build.log.bz2
+
+# Assemble the tarball for the toolchain
+echo "$NERVES_TOOLCHAIN_TAG" > $TARGET_TUPLE/nerves-toolchain.tag
+tar cfz ../../gcc-nerves-$TARGET_TUPLE-linux-$NERVES_TOOLCHAIN_TAG.tgz $TARGET_TUPLE
