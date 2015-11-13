8ba7ae4ab9f9fa59faeb33f21e09588807fddbf4
Frank Hunleth <fhunleth@troodon-software.com>
2015-11-13 08:32:14 -0500

    Fix Erlang relative paths in a way that works on OSX

--- scripts/fix-erlang-abspaths.py
+++ scripts/fix-erlang-abspaths.py
@@ -16,17 +16,33 @@ scriptfile = os.path.abspath(sys.argv[2])
 scriptdir = os.path.dirname(scriptfile)
 tmpfile = scriptfile + '.out'
 
+# Script for resolving the path to scriptfile at call-time
+# See http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+# (readlink -f doesn't work on Mac or this would be substantially simpler)
+script="""#!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+"""
+
 # Search for directories under this base directory
 dirpattern = basedir + '[/a-zA-Z0-9-]+'
 
 with open(scriptfile, 'r') as f:
     with open(tmpfile, 'w') as g:
+	# replace shebang with the script to determine source directory
+	f.readline()
+	g.write(script)
         for line in f:
             m = re.search(dirpattern, line)
             if m:
                 # Assume one absolute to relative conversion per line
                 relpath = os.path.relpath(m.group(0), scriptdir)
-                line = line.replace(m.group(0), "$(dirname $(readlink -f $0))/" + relpath)
+                line = line.replace(m.group(0), "${DIR}/" + relpath)
             g.write(line)
 
 st = os.stat(scriptfile)
