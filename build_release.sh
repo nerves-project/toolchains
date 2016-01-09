2ca37b755b01e8a209eddbd9f5e6821150f26427
Frank Hunleth <fhunleth@troodon-software.com>
2016-01-09 11:44:05 -0500

    Enable x86 toolchain

--- build_release.sh
+++ build_release.sh
@@ -5,7 +5,8 @@ echo "Going to build version '$(git describe --dirty)'. Hit CTRL-C to stop..."
 sleep 2
 
 CONFIGS="glibc-eabihf \
-         glibc-eabihf-armv6"
+         glibc-eabihf-armv6 \
+         glibc-i586"
 
 # Comment out musl support since it doesn't build on OSX
 #         musl-eabihf-armv6"
