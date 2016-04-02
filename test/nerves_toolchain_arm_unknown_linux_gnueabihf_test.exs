92b6aaf23e6ddca76ab7df298ee5eff0898cfb3c
Justin Schneck <jschneck@mac.com>
2016-04-02 18:04:26 -0400

    cleanup

--- test/nerves_toolchain_arm_unknown_linux_gnueabihf_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule NervesToolchainArmUnknownLinuxGnueabihfTest do
-  use ExUnit.Case
-  doctest NervesToolchainArmUnknownLinuxGnueabihf
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
