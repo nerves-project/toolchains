a1c83c934a9fef112413ae393328ca78d73e50ca
Justin Schneck <jschneck@mac.com>
2016-04-02 21:46:41 -0400

    Updated License and Hex information

--- mix.exs
+++ mix.exs
@@ -3,19 +3,31 @@ defmodule NervesToolchainI586UnknownLinuxGnu.Mixfile do
 
   def project do
     [app: :nerves_toolchain_i586_unknown_linux_gnu,
-    version: "0.7.0-dev",
-    elixir: "~> 1.2",
-    compilers: Mix.compilers ++ [:nerves_toolchain],
-    deps: deps]
- end
+     version: "0.7.0-dev",
+     elixir: "~> 1.2",
+     compilers: Mix.compilers ++ [:nerves_toolchain],
+     deps: deps]
+  end
 
- def application do
+  def application do
    [env: [
      target_tuple: "i586-unknown-linux-gnu"
    ]]
- end
+  end
 
- defp deps do
+  defp deps do
    [{:nerves_toolchain, github: "nerves-project/nerves_toolchain"}]
- end
+  end
+
+  defp description do
+   """
+   Nerves Toolchain - i586-unknown-linux-gnu
+   """
+  end
+
+  defp package do
+   [maintainers: ["Frank Hunleth", "Justin Schneck"],
+    licenses: ["Apache 2.0"],
+    links: %{"Github" => "https://github.com/nerves-project/nerves_toolchain_i586_unknown_linux_gnu"}]
+  end
 end
