6f558cd25425caa2eefcb4e5faa973236d78ee5c
Justin Schneck <jschneck@mac.com>
2016-04-02 21:46:49 -0400

    Updated License and Hex information

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule NervesToolchainArmUnknownLinuxGnueabihf.Mixfile do
      version: "0.7.0-dev",
      elixir: "~> 1.2",
      compilers: Mix.compilers ++ [:nerves_toolchain],
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -17,5 +19,17 @@ defmodule NervesToolchainArmUnknownLinuxGnueabihf.Mixfile do
 
   defp deps do
     [{:nerves_toolchain, github: "nerves-project/nerves_toolchain"}]
+  end
+
+  defp description do
+    """
+    Nerves Toolchain - arm-unknown-linux-gnueabihf
+    """
+  end
+
+  defp package do
+    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/nerves-project/nerves_toolchain_arm_unknown_linux_gnueabihf"}]
   end
 end
