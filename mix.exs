5519ca51cd5e772fe4db11df080140ce438401d4
Justin Schneck <jschneck@mac.com>
2016-05-08 17:37:52 -0400

    added nerves manifest file to hex package

--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule NervesToolchainArmUnknownLinuxGnueabihf.Mixfile do
 
   defp package do
     [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     files: ["lib", "src", "README.md", "LICENSE", "nerves.exs"],
      licenses: ["Apache 2.0"],
      links: %{"Github" => "https://github.com/nerves-project/nerves_toolchain_arm_unknown_linux_gnueabihf"}]
   end
