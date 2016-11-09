a8104dfa6e3ef256813697af56546bb0598301ba
Frank Hunleth <fhunleth@troodon-software.com>
2016-11-08 23:00:09 -0500

    Add aarch64 configuration

---
+++ nerves_toolchain_aarch64_unknown_linux_gnueabi/nerves.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+version =
+  Path.join(__DIR__, "VERSION")
+  |> File.read!
+  |> String.strip
+
+package = :nerves_toolchain_aarch64_unknown_linux_gnueabi
+
+config package, :nerves_env,
+  type: :toolchain,
+  version: version,
+  platform: Nerves.Toolchain.CTNG,
+  target_tuple: :aarch64_unknown_linux_gnueabi,
+  artifact_url: [
+    "https://github.com/nerves-project/toolchains/releases/download/v#{version}/#{package}-#{version}.#{Nerves.Env.host_platform}-#{Nerves.Env.host_arch}.tar.xz"
+  ]
