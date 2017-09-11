7f14d45b7bad8bbc6b2bc13b87b06e1c4ca9a3c2
Frank Hunleth <fhunleth@troodon-software.com>
2017-09-10 21:16:43 -0400

    Add x86_64 Musl toolchain
    This one seems to be 2 MB smaller (10% on a trivial Phoenix app) than
    the equivalent glibc toolchain.

---
+++ nerves_toolchain_x86_64_nerves_linux_musl/nerves.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+version =
+  Path.join(__DIR__, "VERSION")
+  |> File.read!
+  |> String.trim
+
+app = :nerves_toolchain_x86_64_nerves_linux_musl
+
+config app, :nerves_env,
+  type: :toolchain,
+  version: version,
+  compiler: :nerves_package,
+  platform: Nerves.Toolchain.CTNG,
+  target_tuple: :x86_64_nerves_linux_musl,
+  artifact_url: [
+    "https://github.com/nerves-project/toolchains/releases/download/v#{version}/#{app}-#{version}.#{Nerves.Env.host_platform}-#{Nerves.Env.host_arch}.tar.xz"
+  ],
+  checksum: [
+    "defconfig",
+    "VERSION"
+  ]
