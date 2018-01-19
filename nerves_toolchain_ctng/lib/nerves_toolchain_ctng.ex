46de1f0ffab61b4501302deb19012f4787f17959
Justin Schneck <jschneck@mac.com>
2018-01-19 11:41:01 -0500

    Update toolchains for nerves v0.9

--- nerves_toolchain_ctng/lib/nerves_toolchain_ctng.ex
+++ nerves_toolchain_ctng/lib/nerves_toolchain_ctng.ex
@@ -1,3 +1,66 @@
-defmodule Nerves.Toolchain.Ctng do
+defmodule Nerves.Toolchain.CTNG do
+  use Nerves.Package.Platform
+
+  alias Nerves.Artifact
+  import Mix.Nerves.Utils
+
+  def bootstrap(_pkg) do
+    :ok
+  end
+
+  def build(pkg, _toolchain, _opts) do
+    build_path = Artifact.build_path(pkg)
+    File.mkdir_p!(build_path)
+    script = 
+      :nerves_toolchain_ctng
+      |> Nerves.Env.package()
+      |> Map.get(:path)
+      |> Path.join("build.sh")
+
+    defconfig = defconfig(pkg)
+
+    case shell(script, [defconfig, build_path]) do
+      {_, 0} -> 
+        x_tools = Path.join(build_path, "x-tools")
+        tuple = 
+          x_tools
+          |> File.ls!
+          |> List.first
+        toolchain_path = Path.join(x_tools, tuple)
+        {:ok, toolchain_path}
+      {error, _} -> {:error, error}
+    end
+  end
+
+  def archive(pkg, _toolchain, _opts) do
+    build_path = Artifact.build_path(pkg)
+    
+    script = 
+      :nerves_toolchain_ctng
+      |> Nerves.Env.package()
+      |> Map.get(:path)
+      |> Path.join("scripts")
+      |> Path.join("archive.sh")
+    
+    tar_path = Path.join([build_path, Artifact.name(pkg) <> Artifact.ext(pkg)])
+
+    case shell(script, [build_path, tar_path]) do
+      {_, 0} -> {:ok, tar_path}
+      {error, _} -> {:error, error}
+    end
+  end
+
+  def clean(pkg) do
+    pkg
+    |> Artifact.dir()
+    |> File.rm_rf()
+  end
+
+  defp defconfig(pkg) do
+    pkg.config
+    |> Keyword.get(:platform_config)
+    |> Keyword.get(:defconfig)
+    |> Path.expand
+  end
   
 end
---
+++ nerves_toolchain_ctng/scripts/archive.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Example
+# 
+# archive.sh /path/to/build_dir /path/to/artifact.tar.xz
+
+
+set -e
+
+WORK_DIR=$(readlink -f $1)
+TARBALL_PATH=$(readlink -f $2)
+
+GCC_INSTALL_DIR=$WORK_DIR/x-tools
+
+BUILD_OS=$(uname -s)
+
+if [[ $BUILD_OS = "darwin" ]]; then
+    # Use GNU tar from Homebrew (brew install gnu-tar)
+    TAR=gtar
+else
+    TAR=tar
+fi
+
+gcc_tuple()
+{
+    # Figure out the target's tuple. It's the name of the only directory.
+    # Don't call this until after build_gcc()
+    tuplepath=$(ls $GCC_INSTALL_DIR)
+    if [[ -e $tuplepath ]]; then
+        echo "unknown"
+    else
+        echo $(basename $tuplepath)
+    fi
+}
+
+echo Building archive...
+
+# Assemble the tarball for the toolchain
+TARGET_TUPLE=$(gcc_tuple)
+TAR_PATH="${TARBALL_PATH/.xz/}"
+TOOLCHAIN_BASE_NAME=$(basename $TARBALL_PATH)
+
+# Save useful information if we ever need to reproduce the toolchain
+#echo "$NERVES_TOOLCHAIN_VERSION" > $GCC_INSTALL_DIR/$TARGET_TUPLE/nerves-toolchain.tag
+#cp $CTNG_CONFIG $GCC_INSTALL_DIR/$TARGET_TUPLE/ct-ng.defconfig
+cp $WORK_DIR/build/.config $GCC_INSTALL_DIR/$TARGET_TUPLE/ct-ng.config
+
+rm -f $TARBALL_PATH $TAR_PATH
+$TAR c -C $GCC_INSTALL_DIR -f $TAR_PATH --transform "s,^$TARGET_TUPLE,$TOOLCHAIN_BASE_NAME," $TARGET_TUPLE
+echo Compressing archive...
+xz $TAR_PATH
