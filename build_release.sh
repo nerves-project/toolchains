e40ae61fb86165ab6357398b48284b6242058397
Frank Hunleth <fhunleth@troodon-software.com>
2018-07-29 12:35:57 -0400

    Add set-version script and refactor toolchain list
    This refactors the toolchain list to one place and updates all the
    convenience scripts to reference it.

--- build_release.sh
+++ build_release.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 
-# Give me a chance to hit CTRL-C in case I'm building a -dirty by accident
-echo "Building a release of all toolchains listed in $0."
+# Load in all of the configs
+. ./support/scripts/all-configs.sh
+
+echo "Building a release of the following toolchain:"
+for CONFIG in $CONFIGS; do
+    echo "  $CONFIG"
+done
+
+echo
 echo "git describe: '$(git describe --dirty)'."
+echo
+
+# Give me a chance to hit CTRL-C in case I'm building a -dirty by accident
 echo "Hit CTRL-C to stop..."
 sleep 2
 
-CONFIGS="\
-    nerves_toolchain_arm_unknown_linux_gnueabihf \
-    nerves_toolchain_x86_64_unknown_linux_musl \
-    nerves_toolchain_armv6_rpi_linux_gnueabi \
-    nerves_toolchain_x86_64_unknown_linux_gnu \
-    nerves_toolchain_armv5tejl_unknown_linux_musleabi \
-    nerves_toolchain_mipsel_unknown_linux_musl \
-    nerves_toolchain_aarch64_unknown_linux_gnueabi \
-    nerves_toolchain_i586_unknown_linux_gnu"
-
 for CONFIG in $CONFIGS; do
-    echo "Starting build for $CONFIG..."
+    echo "Building $CONFIG..."
     # ./nerves_toolchain_ctng/build.sh $CONFIG
     cd $CONFIG
     mix deps.get
