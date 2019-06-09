feb2dcdf1aebeaf1cfcea1437abb6d8bdbd45db0
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-08 22:37:21 -0400

    WIP

---
+++ nerves_toolchain_aarch64_nvidia_linux_gnu/mix.exs
@@ -0,0 +1,88 @@
+defmodule NervesToolchainAarch64NvidiaLinuxGnu.MixProject do
+  use Mix.Project
+
+  @app :nerves_toolchain_aarch64_nvidia_linux_gnu
+  @version Path.join(__DIR__, "VERSION")
+           |> File.read!()
+           |> String.trim()
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.6",
+      compilers: [:nerves_package | Mix.compilers()],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: [loadconfig: [&bootstrap/1]]
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  defp bootstrap(args) do
+    set_target()
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
+  end
+
+  defp nerves_package do
+    [
+      type: :toolchain,
+      platform: Nerves.Toolchain.CTNG,
+      platform_config: [
+        defconfig: "defconfig"
+      ],
+      target_tuple: :aarch64_nvidia_linux_gnu,
+      artifact_sites: [
+        {:github_releases, "nerves-project/toolchains"}
+      ],
+      checksum: package_files()
+    ]
+  end
+
+  defp deps do
+    [
+      {:nerves, "~> 1.4", runtime: false},
+      {:nerves_toolchain_ctng, path: "../nerves_toolchain_ctng", runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Nerves Toolchain - aarch64_nvidia_linux_gnu
+    """
+  end
+
+  defp package do
+    [
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "Github" => "https://github.com/nerves-project/toolchains/tree/master/#{@app}"
+      }
+    ]
+  end
+
+  defp package_files do
+    [
+      "defconfig",
+      "README.md",
+      "LICENSE",
+      "mix.exs",
+      "VERSION"
+    ]
+  end
+
+  defp set_target() do
+    if function_exported?(Mix, :target, 1) do
+      apply(Mix, :target, [:target])
+    else
+      System.put_env("MIX_TARGET", "target")
+    end
+  end
+end
