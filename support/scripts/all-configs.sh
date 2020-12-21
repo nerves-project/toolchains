bc9ae242d18e3978bc61012b1587965a7046b5e1
Justin Schneck <jschneck@mac.com>
2020-12-21 13:34:27 -0500

    Rename armv5tejl to armv5

--- support/scripts/all-configs.sh
+++ support/scripts/all-configs.sh
@@ -5,7 +5,7 @@ CONFIGS="\
     nerves_toolchain_x86_64_nerves_linux_musl \
     nerves_toolchain_armv6_nerves_linux_gnueabi \
     nerves_toolchain_x86_64_nerves_linux_gnu \
-    nerves_toolchain_armv5tejl_nerves_linux_musleabi \
+    nerves_toolchain_armv5_nerves_linux_musleabi \
     nerves_toolchain_mipsel_nerves_linux_musl \
     nerves_toolchain_aarch64_nerves_linux_gnu \
     nerves_toolchain_i586_nerves_linux_gnu"
