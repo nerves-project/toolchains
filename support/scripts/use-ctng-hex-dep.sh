5ffbf416d0e10c23ea815d0c2a9a80dbe50b259d
Justin Schneck <jschneck@mac.com>
2020-12-27 12:04:20 -0500

    Use gsed in helper scrips on mac

--- support/scripts/use-ctng-hex-dep.sh
+++ support/scripts/use-ctng-hex-dep.sh
@@ -4,10 +4,17 @@ set -e
 
 . ./support/scripts/all-configs.sh
 
+BUILD_OS=$(uname -s)
+if [[ $BUILD_OS = "Darwin" ]]; then
+    SED=gsed
+else
+    SED=sed
+fi
+
 for CONFIG in $CONFIGS; do
     echo "Updating deps for $CONFIG..."
     cd $CONFIG
-    sed -ri 's/nerves_toolchain_ctng, path: "..\/nerves_toolchain_ctng"/nerves_toolchain_ctng, "~> 1.7.1"/' mix.exs
+    $SED -ri 's/nerves_toolchain_ctng, path: "..\/nerves_toolchain_ctng"/nerves_toolchain_ctng, "~> 1.7.1"/' mix.exs
     rm mix.lock
     mix deps.update --all
     cd ../
