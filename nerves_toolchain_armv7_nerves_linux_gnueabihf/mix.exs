f55353166416e4c543add6503859412dda785c31
Justin Schneck <jschneck@mac.com>
2020-12-29 14:00:35 -0500

    nerves_toolchain_ctng: bump to v1.8.1

--- nerves_toolchain_armv7_nerves_linux_gnueabihf/mix.exs
+++ nerves_toolchain_armv7_nerves_linux_gnueabihf/mix.exs
@@ -48,7 +48,7 @@ defmodule NervesToolchainArmV7NervesLinuxGnueabihf.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.0", runtime: false},
-      {:nerves_toolchain_ctng, "~> 1.8.0", runtime: false}
+      {:nerves_toolchain_ctng, "~> 1.8.1", runtime: false}
     ]
   end
 
