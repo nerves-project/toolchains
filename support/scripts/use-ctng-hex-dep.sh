9c766bdd73a422d5fcfbb9e7e8a2d16916d47edc
Frank Hunleth <fhunleth@troodon-software.com>
2021-12-13 09:09:59 -0500

    Use hex for nerves_toolchain_ctng

--- support/scripts/use-ctng-hex-dep.sh
+++ support/scripts/use-ctng-hex-dep.sh
@@ -14,7 +14,7 @@ fi
 for CONFIG in $CONFIGS; do
     echo "Updating deps for $CONFIG..."
     cd $CONFIG
-    $SED -ri 's/nerves_toolchain_ctng, path: "..\/nerves_toolchain_ctng"/nerves_toolchain_ctng, "~> 1.8.4"/' mix.exs
+    $SED -ri 's/nerves_toolchain_ctng, path: "..\/nerves_toolchain_ctng"/nerves_toolchain_ctng, "~> 1.8.5"/' mix.exs
     rm mix.lock
     mix deps.update --all
     cd ../
