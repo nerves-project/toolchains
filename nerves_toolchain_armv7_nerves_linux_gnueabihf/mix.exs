1514af4d449a144ec1f34d04338eea351c47f5af
Frank Hunleth <fhunleth@troodon-software.com>
2022-06-20 18:24:37 -0400

    Bump nerves_toolchain_ctng reference

--- nerves_toolchain_armv7_nerves_linux_gnueabihf/mix.exs
+++ nerves_toolchain_armv7_nerves_linux_gnueabihf/mix.exs
@@ -48,7 +48,7 @@ defmodule NervesToolchainArmV7NervesLinuxGnueabihf.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.0", runtime: false},
-      {:nerves_toolchain_ctng, "~> 1.8.5", runtime: false}
+      {:nerves_toolchain_ctng, "~> 1.9.0", runtime: false}
     ]
   end
 
