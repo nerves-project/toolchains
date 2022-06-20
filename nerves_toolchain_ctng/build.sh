6effc2a79da9e28d4eca69b044af4cf9550a0d82
Frank Hunleth <fhunleth@troodon-software.com>
2022-06-19 22:41:30 -0400

    crosstool-ng: bump to latest (gcc 12.1)

--- nerves_toolchain_ctng/build.sh
+++ nerves_toolchain_ctng/build.sh
@@ -9,7 +9,7 @@ set -e
 # Set CTNG_USE_GIT=true to use git to download the release (only needed for non-released ct-ng builds)
 
 CTNG_USE_GIT=true
-CTNG_TAG=584e57e888fd652ff6228c1dbdff18556149c7cb
+CTNG_TAG=618affc78902e86815c908e08a1d0c51832ce7ce
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
