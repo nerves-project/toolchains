560ce67cede21615807c21f0e090c299a2fa66a7
Frank Hunleth <fhunleth@troodon-software.com>
2022-12-14 15:08:39 -0500

    Use ctng path to verify static toolchain/LTO change

--- nerves_toolchain_armv7_nerves_linux_musleabihf/mix.exs
+++ nerves_toolchain_armv7_nerves_linux_musleabihf/mix.exs
@@ -48,7 +48,7 @@ defmodule NervesToolchainArmV7NervesLinuxMusleabihf.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.0", runtime: false},
-      {:nerves_toolchain_ctng, "~> 1.9.0", runtime: false}
+      {:nerves_toolchain_ctng, path: "../nerves_toolchain_ctng", runtime: false}
     ]
   end
 
