bb4cf0ed5fa4c0d0dd59b2d0192b47d6fbae69d9
Frank Hunleth <fhunleth@troodon-software.com>
2024-08-18 19:53:45 -0400

    crosstool-ng: bump to ed12fa6 (Aug 5, 2024)
    This brings in GCC 14.2.

--- nerves_toolchain_ctng/build.sh
+++ nerves_toolchain_ctng/build.sh
@@ -9,7 +9,7 @@ set -e
 # Set CTNG_USE_GIT=true to use git to download the release (only needed for non-released ct-ng builds)
 
 CTNG_USE_GIT=true
-CTNG_TAG=daf19e17615fdc58503ead8ebff252d7ade90fc0
+CTNG_TAG=ed12fa68402f58e171a6f79500f73f4781fdc9e5
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
