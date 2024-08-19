bb0fe0046b458cc825a8bfde9435d2456a4ad287
Frank Hunleth <fhunleth@troodon-software.com>
2024-08-18 21:00:11 -0400

    Bump gcc for Homebrew to GCC 14
    [skip-ci]

--- nerves_toolchain_ctng/build.sh
+++ nerves_toolchain_ctng/build.sh
@@ -242,9 +242,9 @@ build_gcc()
 	gmake
 	gmake install
     elif [[ $BUILD_OS = "darwin" ]]; then
-        # Homebrew's gcc is gcc-13
+        # Homebrew's gcc is gcc-14
         BINUTILS=$(brew --prefix binutils)
-        CC=gcc-13 CXX=g++-13 OBJDUMP=$BINUTILS/bin/gobjdump OBJCOPY=$BINUTILS/bin/gobjcopy READELF=$BINUTILS/bin/greadelf \
+        CC=gcc-14 CXX=g++-14 OBJDUMP=$BINUTILS/bin/gobjdump OBJCOPY=$BINUTILS/bin/gobjcopy READELF=$BINUTILS/bin/greadelf \
 	    CFLAGS="$CROSSTOOL_CFLAGS" LDFLAGS="$CROSSTOOL_LDFLAGS" SED=$HOMEBREW_PREFIX/bin/gsed MAKE=$HOMEBREW_PREFIX/bin/gmake ./configure --prefix="$LOCAL_INSTALL_DIR"
         SED=$HOMEBREW_PREFIX/bin/gsed
 	gmake
